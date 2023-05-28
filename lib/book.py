70d84545eb05a9cfc304dd7507d52a4b30fddf44
Rolex Omondi <rolexomondi46@gmail.com>
2023-05-28 12:16:57 +0300

    Completed assignmnent

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 class Book:
-    pass
-    
+    def __init__(self,title,page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
         
+
+    def get_pagec(self):
+        print("retrieving pagecount")
+        return self._page_count
+    
+    def set_pagec(self,page_count):
+        if isinstance(page_count, int):
+            print(f"setting page_count to {page_count}")
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_pagec,set_pagec)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!" )
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        print("retrieving size")
+        return self._size
+    def set_size(self,size):
+        if isinstance(size,int):
+            print(f"setting size to {size}")
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size,set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
